a44fc0869a8af001b92f06eeaa8a7aa8155d8923
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-23 17:10:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"  # Replace with your BotFather token
+RAPIDAPI_KEY = ""  # Optional: Add RapidAPI key for PrepAI from https://rapidapi.com/DataToBiz/api/prepai-generate-questions/
